c3f0039adbe5dd7712b9667f6c0a41c2a23ab1cb
Artem Aguichine <artyomaguichine@gmail.com>
2024-08-22 17:29:37 -0700

    Update mardigras.py

--- mardigras.py
+++ mardigras.py
@@ -1,15 +1,18 @@
+##############################################
+#
+#   READ FIT COEFFICIENTS
+#
+##############################################
+# %matplotlib inline
+# %matplotlib widget
 import numpy as np
 import matplotlib.pyplot as plt
+# from numpy import genfromtxt
 from matplotlib.widgets import Button, Slider
 from matplotlib import patheffects
 
 from scipy.interpolate import RegularGridInterpolator
 
-##############################################
-#
-#   READ MODELS
-#
-##############################################
 
 #Paths to models
 path_models = "./models/"
@@ -49,13 +52,13 @@ data_d = np.reshape(listd,(10,10,10))
 data_masslimlow = np.reshape(listmasslow,(10,10,10))
 data_masslimhigh = np.reshape(listmasshigh,(10,10,10))
 
-interp_a = RegularGridInterpolator((dimcmf, dimteq, dimwmf), data_a, method='linear', bounds_error=False, fill_value=None)
-interp_b = RegularGridInterpolator((dimcmf, dimteq, dimwmf), data_b, method='linear', bounds_error=False, fill_value=None)
-interp_c = RegularGridInterpolator((dimcmf, dimteq, dimwmf), data_c, method='linear', bounds_error=False, fill_value=None)
-interp_d = RegularGridInterpolator((dimcmf, dimteq, dimwmf), data_d, method='linear', bounds_error=False, fill_value=None)
+interp_a = RegularGridInterpolator((dimcmf, dimteq, dimwmf), data_a, method='cubic', bounds_error=False, fill_value=None)
+interp_b = RegularGridInterpolator((dimcmf, dimteq, dimwmf), data_b, method='cubic', bounds_error=False, fill_value=None)
+interp_c = RegularGridInterpolator((dimcmf, dimteq, dimwmf), data_c, method='cubic', bounds_error=False, fill_value=None)
+interp_d = RegularGridInterpolator((dimcmf, dimteq, dimwmf), data_d, method='cubic', bounds_error=False, fill_value=None)
 
-interp_masslimlow = RegularGridInterpolator((dimcmf, dimteq, dimwmf), data_masslimlow, method='linear', bounds_error=False, fill_value=None)
-interp_masslimhigh = RegularGridInterpolator((dimcmf, dimteq, dimwmf), data_masslimhigh, method='linear', bounds_error=False, fill_value=None)
+interp_masslimlow = RegularGridInterpolator((dimcmf, dimteq, dimwmf), data_masslimlow, method='cubic', bounds_error=False, fill_value=None)
+interp_masslimhigh = RegularGridInterpolator((dimcmf, dimteq, dimwmf), data_masslimhigh, method='cubic', bounds_error=False, fill_value=None)
 
 
 ##############################################
@@ -115,13 +118,10 @@ interp_lf14 = RegularGridInterpolator((dim_met_lf14, dim_age_lf14, dim_teq_lf14,
 # Exoplanet catalog
 
 # Update the catalog by copy/pasting the content from the NASA Exoplanet Archive's Table Access Protocol (TAP)
-# https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+pl_name,pl_rade,pl_radeerr1,pl_radeerr2,pl_masse,pl_masseerr1,pl_masseerr2,pl_eqt+from+ps+where+default_flag=1&format=tsv
-#
-# or this link if you want to use best planet mass estimate (mass, m*sin(i), or m*sin(i)/sin(i)):
-# https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+pl_name,pl_rade,pl_radeerr1,pl_radeerr2,pl_bmasse,pl_bmasseerr1,pl_bmasseerr2,pl_eqt+from+ps+where+default_flag=1&format=tsv
+# https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+pl_name,pl_rade,pl_radeerr1,pl_radeerr2,pl_masse,pl_masseerr1,pl_masseerr2,pl_eqt+from+ps+where+default_flag=1+and+pl_controv_flag=0+and+pl_rade+is+not+null+and+pl_masse+is+not+null+and+pl_bmassprov='Mass'&format=tsv
 
 list_catalog_rp,list_catalog_rpe1,list_catalog_rpe2,list_catalog_mp,list_catalog_mpe1,list_catalog_mpe2 \
-    = np.genfromtxt("./data/catalog_exoplanets.dat",skip_header=10,delimiter="\t",unpack=True,usecols=(1,2,3,4,5,6),filling_values=0.0)
+    = np.genfromtxt("./data/catalog_exoplanets.dat",delimiter="\t",unpack=True,usecols=(1,2,3,4,5,6),filling_values=0.0)
 
 # This procedure removes planets that don't have radius and/or mass measurements
 # the goal is to have arrays of smaller size, so that rendering is faster when sliders are used
@@ -132,7 +132,10 @@ list_exo_mp = []
 list_exo_mpe1 = []
 list_exo_mpe2 = []
 for i in range(len(list_catalog_rp)):
-    if list_catalog_rp[i]!=0.0 and list_catalog_mp[i]!=0.0:
+    exo_mass_prec = 0.5 # minimum precision on exoplanet mass
+    if list_catalog_rp[i]!=0.0 and list_catalog_mp[i]!=0.0 \
+        and (abs(list_catalog_mpe1[i]))/list_catalog_mp[i] < exo_mass_prec \
+        and (abs(list_catalog_mpe2[i]))/list_catalog_mp[i] < exo_mass_prec:
         list_exo_rp = np.append(list_exo_rp,[list_catalog_rp[i]])
         list_exo_rpe1 = np.append(list_exo_rpe1,[list_catalog_rpe1[i]])
         list_exo_rpe2 = np.append(list_exo_rpe2,[list_catalog_rpe2[i]])
@@ -140,12 +143,21 @@ for i in range(len(list_catalog_rp)):
         list_exo_mpe1 = np.append(list_exo_mpe1,[list_catalog_mpe1[i]])
         list_exo_mpe2 = np.append(list_exo_mpe2,[list_catalog_mpe2[i]])
 
-
 # Targets catalog
 # The intended use is to showcase a few targets (dedicated study, new discovery, update of parameters, etc.)
 # The catalog of targets must have the same formatting as the exoplanet catalog.
 list_targets_rp,list_targets_rpe1,list_targets_rpe2,list_targets_mp,list_targets_mpe1,list_targets_mpe2\
-    = np.genfromtxt("./data/catalog_targets.dat",skip_header=10,delimiter="\t",unpack=True,usecols=(1,2,3,4,5,6),filling_values=0.0)
+    = np.genfromtxt("./data/catalog_targets.dat",delimiter="\t",unpack=True,usecols=(1,2,3,4,5,6),filling_values=0.0)
+
+import csv
+file_path = "./data/catalog_targets.dat"
+list_targets_names = []
+i=0
+with open(file_path, 'r') as file:
+    reader = csv.reader(file, delimiter='\t')
+    for row in reader:
+        i+=1
+        if i > 10: list_targets_names.append(row[0])  # Append the first column (label) to the list
 
 
 ##############################################
@@ -247,18 +259,19 @@ init_fenv_lf = 1.0
 init_cmf_zeng = 0.325
 
 
-# Create the figure and the sliding lines
-fig, ax = plt.subplots()
+# Create the figure
+fig = plt.figure(figsize=(7,7))
+
+# make axes for main figure
+ax = fig.add_axes([0.11, 0.1, 0.83, 0.6])  # [left, bottom, width, height]
 
 # Figure options
-fig.set_figheight(7)
-fig.set_figwidth(7)
-plt.xscale("log")
-plt.xlim(xmin, xmax)
-plt.ylim(ymin, ymax)
-plt.xlabel('Mass [Me]')
-plt.ylabel('Radius [Re]')
-plt.grid(visible=True,which='major', axis='both')
+ax.set_xscale("log")
+ax.set_xlim(xmin, xmax)
+ax.set_ylim(ymin, ymax)
+ax.set_xlabel('Mass [Me]')
+ax.set_ylabel('Radius [Re]')
+ax.grid(visible=True,which='major', axis='both')
 
 # Sliding lines
 line_iop, = ax.plot(x, rad_iop(x, init_cmf_iop,init_wmf_iop,init_teq_iop),lw=2,color='blue',ls='--',zorder=20)
@@ -278,6 +291,7 @@ ax.errorbar(list_exo_mp,list_exo_rp,
             xerr=list_exo_mpe,
             fmt='o',zorder=-30,
             c="black",alpha=0.2)
+
 # Exoplanets to be highlighted
 ax.errorbar(list_targets_mp,list_targets_rp,
             yerr=list_targets_rpe,
@@ -285,16 +299,34 @@ ax.errorbar(list_targets_mp,list_targets_rp,
             ls='',c='orange',elinewidth=3,
             marker='*',mfc='orange',mec='black', ms=15, mew=1,
             zorder=50)
+
+# Annotate each point
+texts = []
+# Use these to expand annotations horizontally and/or vertically
+text_expand_h = False
+text_expand_v = True
+for i, label in enumerate(list_targets_names):
+    x_text = 10
+    y_text = -15
+    if text_expand_h: x_text = ((list_targets_mp[i]-np.median(list_targets_mp))/(max(list_targets_mp)-min(list_targets_mp)))*15
+    if text_expand_v: y_text = ((list_targets_rp[i]-np.median(list_targets_rp))/(max(list_targets_rp)-min(list_targets_rp)))*15
+    texts.append(ax.annotate(
+    label, 
+    (list_targets_mp[i], list_targets_rp[i]), 
+    textcoords="offset points", 
+    xytext=(x_text,y_text), 
+    ha='left',
+    fontsize='x-small',
+    bbox=dict(boxstyle="round,pad=0.3", edgecolor='black', facecolor='white', alpha=0.7),zorder=50+i
+    ))
+    #plt.annotate(label, (list_targets_mp[i], list_targets_rp[i]), textcoords="offset points", xytext=(5,-10),
+    #ha='left',bbox=dict(boxstyle="round,pad=0.3", edgecolor='black', facecolor='white', alpha=0.7),zorder=50+i)
+
 # Solar system planets
 for i in range(len(ss_planets)):
-    text = plt.text(ss_masses[i], ss_radii[i], ss_alchemy_symbols[ss_planets[i]], \
+    text = ax.text(ss_masses[i], ss_radii[i], ss_alchemy_symbols[ss_planets[i]], \
             fontsize=15, ha='center', va='center', color='red')
     text.set_path_effects([patheffects.withStroke(linewidth=1, foreground='black')])  # Add outline to text
-
-# ax.errorbar(list_ssystem_mp,list_ssystem_rp,
-#             ls='',c='red',
-#             marker='o',mfc='red',mec='black', ms=5, mew=1,
-#             zorder=35)
 
 # Zeng fixed
 zeng_width=1.0
@@ -304,102 +336,7 @@ ax.plot(list_zeng_masses,list_zeng_mg_radii,linewidth=zeng_width,color='grey',zo
 ax.plot(zeng_mass,zeng_50wat,linewidth=zeng_width,color='cyan',zorder=8)
 ax.plot(zeng_mass,zeng_100wat,linewidth=zeng_width,color='cyan',zorder=9)
 
-
-# adjust the main plot to make room for the sliders
-fig.subplots_adjust(top=0.7)
-
-# Labels
-ax.text(0.05, 0.95, 'Aguichine et al. 2021',weight='bold',
-        transform=fig.transFigure,color='blue',
-        bbox={'ec': 'white', 'fc':'white','color':'blue', 'pad': 10})
-
-# Make a horizontal slider to control the CMF
-ax_cmf_iop = fig.add_axes([0.1, 0.90, 0.15, 0.02])
-cmf_iop_slider = Slider(
-    ax=ax_cmf_iop,
-    label='CMF ',
-    valmin=0.0,
-    valmax=0.9,
-    valinit=init_cmf_iop,
-    valfmt=' %1.3f'
-)
-
-# Make a horizontal oriented slider to control the WMF
-ax_wmf_iop = fig.add_axes([0.1, 0.85, 0.15, 0.02])
-wmf_iop_slider = Slider(
-    ax=ax_wmf_iop,
-    label="WMF ",
-    valmin=0.1,
-    valmax=1.0,
-    valinit=init_wmf_iop,
-    valfmt=' %1.3f'
-)
-
-# Make a horizontal oriented slider to control the Teq
-ax_teq_iop = fig.add_axes([0.1, 0.80, 0.15, 0.02])
-teq_iop_slider = Slider(
-    ax=ax_teq_iop,
-    label="Teq [K] ",
-    valmin=400.0,
-    valmax=1300.0,
-    valinit=init_teq_iop,
-    valfmt=' %4.0f'
-)
-
-# Labels
-ax.text(0.38, 0.95, 'Lopez & Fortney 2014',weight='bold',
-        transform=fig.transFigure,color='red',
-        bbox={'ec': 'white', 'fc':'white','color':'blue', 'pad': 10})
-
-# Make a horizontal slider to control the Metallicity
-ax_met_lf = fig.add_axes([0.45, 0.90, 0.15, 0.02])
-met_lf_slider = Slider(
-    ax=ax_met_lf,
-    label='M [Sun] ',
-    valmin=1.0,
-    valmax=50.0,
-    valinit=init_met_lf,
-    valfmt=' %2.0f'
-)
-
-# Make a horizontal oriented slider to control the Age
-ax_age_lf = fig.add_axes([0.45, 0.85, 0.15, 0.02])
-age_lf_slider = Slider(
-    ax=ax_age_lf,
-    label="Age [Gyr] ",
-    valmin=0.1,
-    valmax=10.0,
-    valinit=init_age_lf,
-    valfmt=' %2.1f'
-)
-
-# Make a horizontal oriented slider to control the Teq
-ax_teq_lf = fig.add_axes([0.45, 0.80, 0.15, 0.02])
-teq_lf_slider = Slider(
-    ax=ax_teq_lf,
-    label="Teq [K] ",
-    valmin=160.0,
-    valmax=1500.0,
-    valinit=init_teq_lf,
-    valfmt=' %4.0f'
-)
-
-# Make a horizontal oriented slider to control the Envelope fraction
-ax_fenv_lf = fig.add_axes([0.45, 0.75, 0.15, 0.02])
-fenv_lf_slider = Slider(
-    ax=ax_fenv_lf,
-    label="f [%] ",
-    valmin=0.01,
-    valmax=20.0,
-    valinit=init_fenv_lf,
-    valfmt=' %2.2f'
-)
-
-# Labels
-ax.text(0.75, 0.95, 'Zeng et al. 2016',weight='bold',
-        transform=fig.transFigure,color='brown',
-        bbox={'ec': 'white', 'fc':'white','color':'blue', 'pad': 10})
-
+# Zeng fixed labels
 ax.text(0.55, 0.60, '100% Core',fontsize=5,
         color='black',rotation=10)
         
@@ -416,8 +353,112 @@ ax.text(0.55, 1.23, '100% Liquid H2O',fontsize=5,
         color='blue',rotation=17)
 
 
+##############################################
+#
+#   MAKE SLIDERS
+#
+##############################################
+
+# Aguichine+2021 Slider
+
+# Label
+fig.text(0.05, 0.95, 'Aguichine et al. 2021',weight='bold',
+        color='blue',
+        bbox={'ec': 'white', 'fc':'white','color':'blue', 'pad': 10})
+
 # Make a horizontal slider to control the CMF
-ax_cmf_zeng = fig.add_axes([0.75, 0.90, 0.15, 0.02])
+ax_cmf_iop = fig.add_axes([0.1, 0.90, 0.15, 0.02])  # [left, bottom, width, height]
+cmf_iop_slider = Slider(
+    ax=ax_cmf_iop,
+    label='CMF ',
+    valmin=0.0,
+    valmax=0.9,
+    valinit=init_cmf_iop,
+    valfmt=' %1.3f'
+)
+
+# Make a horizontal oriented slider to control the WMF
+ax_wmf_iop = fig.add_axes([0.1, 0.85, 0.15, 0.02])  # [left, bottom, width, height]
+wmf_iop_slider = Slider(
+    ax=ax_wmf_iop,
+    label="WMF ",
+    valmin=0.1,
+    valmax=1.0,
+    valinit=init_wmf_iop,
+    valfmt=' %1.3f'
+)
+
+# Make a horizontal oriented slider to control the Teq
+ax_teq_iop = fig.add_axes([0.1, 0.80, 0.15, 0.02])  # [left, bottom, width, height]
+teq_iop_slider = Slider(
+    ax=ax_teq_iop,
+    label="Teq [K] ",
+    valmin=400.0,
+    valmax=1300.0,
+    valinit=init_teq_iop,
+    valfmt=' %4.0f'
+)
+
+# Lopez & Fortney 2014 Slider
+
+# Label
+fig.text(0.38, 0.95, 'Lopez & Fortney 2014',weight='bold',
+        color='red',
+        bbox={'ec': 'white', 'fc':'white','color':'blue', 'pad': 10})
+
+# Make a horizontal slider to control the Metallicity
+ax_met_lf = fig.add_axes([0.45, 0.90, 0.15, 0.02])  # [left, bottom, width, height]
+met_lf_slider = Slider(
+    ax=ax_met_lf,
+    label='M [Sun] ',
+    valmin=1.0,
+    valmax=50.0,
+    valinit=init_met_lf,
+    valfmt=' %2.0f'
+)
+
+# Make a horizontal oriented slider to control the Age
+ax_age_lf = fig.add_axes([0.45, 0.85, 0.15, 0.02])  # [left, bottom, width, height]
+age_lf_slider = Slider(
+    ax=ax_age_lf,
+    label="Age [Gyr] ",
+    valmin=0.1,
+    valmax=10.0,
+    valinit=init_age_lf,
+    valfmt=' %2.1f'
+)
+
+# Make a horizontal oriented slider to control the Teq
+ax_teq_lf = fig.add_axes([0.45, 0.80, 0.15, 0.02])  # [left, bottom, width, height]
+teq_lf_slider = Slider(
+    ax=ax_teq_lf,
+    label="Teq [K] ",
+    valmin=160.0,
+    valmax=1500.0,
+    valinit=init_teq_lf,
+    valfmt=' %4.0f'
+)
+
+# Make a horizontal oriented slider to control the Envelope fraction
+ax_fenv_lf = fig.add_axes([0.45, 0.75, 0.15, 0.02])  # [left, bottom, width, height]
+fenv_lf_slider = Slider(
+    ax=ax_fenv_lf,
+    label="f [%] ",
+    valmin=0.01,
+    valmax=20.0,
+    valinit=init_fenv_lf,
+    valfmt=' %2.2f'
+)
+
+# Zeng+2016 Slider
+
+# Label
+fig.text(0.75, 0.95, 'Zeng et al. 2016',weight='bold',
+        color='brown',
+        bbox={'ec': 'white', 'fc':'white','color':'blue', 'pad': 10})
+
+# Make a horizontal slider to control the CMF
+ax_cmf_zeng = fig.add_axes([0.75, 0.90, 0.15, 0.02])  # [left, bottom, width, height]
 cmf_zeng_slider = Slider(
     ax=ax_cmf_zeng,
     label='CMF ',
@@ -435,7 +476,6 @@ def update(val):
     line_lf.set_ydata(rad_lf(x, met_lf_slider.val,age_lf_slider.val,teq_lf_slider.val,fenv_lf_slider.val))
     line_zeng.set_ydata(rad_zeng(x, cmf_zeng_slider.val))
 
-
 # register the update function with each slider
 cmf_iop_slider.on_changed(update)
 wmf_iop_slider.on_changed(update)
@@ -447,7 +487,7 @@ fenv_lf_slider.on_changed(update)
 cmf_zeng_slider.on_changed(update)
 
 # Create a `matplotlib.widgets.Button` to reset the sliders to initial values.
-resetax = fig.add_axes([0.8, 0.025, 0.1, 0.04])
+resetax = fig.add_axes([0.84, 0.025, 0.1, 0.04])  # [left, bottom, width, height]
 button = Button(resetax, 'Reset', hovercolor='0.975')
 
 
@@ -460,7 +500,6 @@ def reset(event):
     teq_lf_slider.reset()
     fenv_lf_slider.reset()
     cmf_zeng_slider.reset()
-
 
 button.on_clicked(reset)
 
